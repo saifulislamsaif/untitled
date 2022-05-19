f3e555be296aedb9e3fd1ea32acd99ffe91f596a
saiful islam <saifulislambaust28@gmail.com>
2022-05-19 18:02:25 +0600

    another modification done for dfs

--- dfs.cpp
+++ dfs.cpp
@@ -1,53 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int N = 1e5 + 10;
+const int N = 1e7 + 10;
 vector<int> g[N];
 bool vis[N];
 
 void dfs(int vertex) {
-    /**Take action on vertex after entering the vertex
-     *
-     **/
-    cout << vertex << endl;
-//     if(vis[vertex]) return;
+
     vis[vertex] = true;
     for (int child: g[vertex]) {
-        cout << "par " << vertex << ", child " << child << endl;
-        /**Take action on child before
-         * entering the child node
-         **/
         if (vis[child]) continue;
         dfs(child);
-        /**Take action on child
-         * after exiting child node
-         */
+
     }
-    /*
-     * Take action on vertex before
-     * before exiting the vertex
-     **/
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
-    for (int i = 0; i < 9; ++i) {
+    for (int i = 0; i < m; ++i) {
         int v1, v2;
         cin >> v1 >> v2;
         g[v1].push_back(v2);
         g[v2].push_back(v1);
     }
-    dfs(1);
- }
-//
-//6 9
-//1 3
-//1 5
-//3 5
-//3 4
-//3 6
-//3 2
-//2 6
-//4 6
-//5 6
+    int ct = 0;
+    for (int i = 1; i <= n ; ++i) {
+        if(vis[i]) continue;
+        dfs(i);
+        ct++;
+    }
+    cout<<ct<<endl;
+}
